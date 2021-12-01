8e861e011bc7e9ae653315c6c1d0904bce2eb36e
Andrysalva <a.salvatori38@gmail.com>
2021-12-01 12:37:04 +0100

    crato toast

--- app/src/main/java/com/example/esegiteam5b/MainActivity.java
+++ app/src/main/java/com/example/esegiteam5b/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.esegiteam5b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        String testo = "toast";
+        Toast toast = Toast.makeText(context,testo, duration);
+        toast.show();
     }
 }
