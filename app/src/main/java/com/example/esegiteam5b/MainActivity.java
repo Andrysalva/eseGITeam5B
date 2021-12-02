4ac5a05849477aa50e1d990b197936170559bf84
Daniele Pandolfo <cocomelone46@gmail,com>
2021-12-02 15:09:04 +0100

    danieleToastNew

--- app/src/main/java/com/example/esegiteam5b/MainActivity.java
+++ app/src/main/java/com/example/esegiteam5b/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String t="Danieleee"
+        String t="Daniele Pandolfoooo"
         Toast to = Toast.makeText(getApplicationContext(), t, Toast.LENGTH_SHORT);
         to.show();
     }
